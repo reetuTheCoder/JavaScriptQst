8f2f9560558ca255aa63a66ab0563a1e7d1336b1
reetuTheCoder <codewithreetu@gmail.com>
2025-12-14 18:38:18 +0530

    when we dont want a duplicate element in our increasing sub array

--- QstBasedFile/day2/longestIncSubarrNtRep.js
+++ QstBasedFile/day2/longestIncSubarrNtRep.js
@@ -1,5 +1,8 @@
 // let arr = ["x", "x", "z", "z", "y", "x"];
-let arr = ["x", "y", "z"];
+// let arr = ["x", "y", "z"];
+// let arr = ["x", "y", "z", "z", "a", "b", "b", "c"];
+let arr = ["a", "b", "c", "c", "d"];
+
 
 // [x]
 // [x,y]
@@ -9,22 +12,26 @@ let arr = ["x", "y", "z"];
 // [z]
 
 let longSubArr = [];
-let maxlen = 0;
+let maxlen = 1;
 
 for (let i = 0; i < arr.length; i++) {
-  let sub = [arr[i]]; // stored i first ele
+  let sub = [arr[i]];
+  let checkEle = new Set([arr[i]]);
+
+  // console.log("check", checkEle);
 
   //   console.log("sub", sub);
 
   for (let j = i + 1; j < arr.length; j++) {
     // console.log(arr[j],"dfkjdskfj", arr[j-1]);
 
-    if (arr[j] > arr[j - 1]) {
+    if (arr[j] > arr[j - 1] && !checkEle.has(arr[j])) {
       sub.push(arr[j]);
+      checkEle.add(arr[j]);
 
-      //  console.log("sub111", sub);
+      // console.log("check", checkEle);
     } else {
-      break; // 🔑 stop when order breaks
+      break;
     }
 
     if (sub.length > maxlen) {
